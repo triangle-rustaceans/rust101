cf035da731a9078f9b8ea1331c2ae030be183154
J. Cliff Dyer <jcd@sdf.org>
2018-01-14 22:32:21 -0500

    Working cyoa with data structures

--- cyoa/src/main.rs
+++ cyoa/src/main.rs
@@ -5,17 +5,19 @@ extern crate serde_json;
 
 use serde_json::Value;
 use std::fs::File;
+use std::io::prelude::*;
+use std::io::stdin;
 
 pub mod cyoa {
     #[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
     pub struct CYOA {
         pub title: String,
         pub author: String,
-        pub locations: Vec<CYOAPage>,
+        pub locations: Vec<CYOALocation>,
     }
 
     #[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
-    pub struct CYOAPage {
+    pub struct CYOALocation {
         pub id: u64,
         pub text: String,
         pub actions: Option<Vec<CYOAAction>>,
@@ -29,11 +31,55 @@ pub mod cyoa {
     }
 }
 
+fn page(cyoa: &cyoa::CYOA, index: u64) -> Option<u64> {
+    let location = cyoa.locations.get((index) as usize)?;
+    println!("\n{}", &location.text);
+    let mut i = 1;
+    let action_vec = match &location.actions {
+        &Some(ref action_vec) => action_vec,
+        &None => return None,
+    };
+    println!();
+    for action in action_vec {
+        println!("{}) {} ({})", i, action.text, action.label);
+        i += 1;
+    }
+    let mut choice = String::with_capacity(64);
+    let mut next_action = None;
+    while next_action.is_none() {
+        choice.truncate(0);
+        stdin().read_line(&mut choice).expect("reading input");
+        let choice = choice.trim();
+        next_action = match choice.parse::<u64>() {
+            Ok(n) => action_vec.get(n as usize - 1),
+            Err(_) => action_vec.iter().find(|action| action.label == choice),
+        };
+    }
+    println!("{:?}", next_action.unwrap().text);
+    next_action.map(|action| action.target)
+}
+
+fn begin(cyoa: &cyoa::CYOA) {
+    let mut next = page(cyoa, 0);
+    while next.is_some() {
+        next = page(cyoa, next.unwrap());
+    }
+    println!("The end.");
+}
+
 fn main() {
-    let reader = File::open("../data/adventure.json").expect("data file");
-    let res: Result<cyoa::CYOA, _> = serde_json::from_reader(reader);
+    let mut reader = File::open("../data/adventure.json").expect("data file");
+    let mut s = String::new();
+    reader.read_to_string(&mut s).expect("read file");
+    let res: Result<cyoa::CYOA, _> = serde_json::from_str(&s);
     match res {
-        Ok(value) => println!("{} {}.", value.title, value.author),
-        Err(error) => println!("An Error, {:?}", error),
+        Ok(value) => {
+            println!("{}, by {}.", value.title, value.author);
+            begin(&value);
+        },
+        Err(error) => {
+            println!("An error occurred: {:?}", error);
+            ::std::process::exit(1);
+        },
     };
 }
